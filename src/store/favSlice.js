038d80d968be9ccd85dd4734d45fa7fe3db5b770
pitis6 <felipecalvache6@gmail.com>
2023-06-15 22:23:43 -0500

    Creating redux

---
+++ src/store/favSlice.js
@@ -0,0 +1,19 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  myList: [],
+};
+
+export const favSlice = createSlice({
+  name: "favs",
+  initialState,
+  reducers: {
+    addMovie: (state, action) => {
+      state.myList = [...state.myList, action.payload];
+      localStorage.setItem("favsMovies", JSON.stringify(state.myList));
+    },
+  },
+});
+
+export const { addMovie } = favSlice.actions;
+export default favSlice.reducer;
